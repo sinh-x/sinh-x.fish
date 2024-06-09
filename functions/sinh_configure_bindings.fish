cf3dd7fd32a81e07d28b349077e43f250a008a13
Sinh Nguyen <sinh@sinhn.com>
2024-06-09 14:32:29 +0700

    Fish typos when copy from fzf.fish

--- functions/sinh_configure_bindings.fish
+++ functions/sinh_configure_bindings.fish
@@ -1,6 +1,6 @@
 # Always installs bindings for insert and default mode for simplicity and b/c it has almost no side-effect
 # https://gitter.im/fish-shell/fish-shell?at=60a55915ee77a74d685fa6b1
-function fzf_configure_bindings --description "Installs the default key bindings for sinh-x.fish with user overrides passed as options."
+function sinh_configure_bindings --description "Installs the default key bindings for sinh-x.fish with user overrides passed as options."
     # no need to install bindings if not in interactive mode or running tests
     status is-interactive || test "$CI" = true; or return
 
