2d4d783920101f59adc4bcd683b1024fb91f2401
Sinh Nguyen <sinh@sinhn.com>
2024-06-09 15:32:30 +0700

    Copy uninstall from fzf

--- conf.d/sinh-x.fish
+++ conf.d/sinh-x.fish
@@ -3,4 +3,17 @@ if not status is-interactive && test "$CI" != true
     exit
 end
 
+# install the default bindings
 sinh_configure_bindings
+
+# Doesn't erase autoloaded _sinh_* functions because they are not easily accessible once key bindings are erased
+function _sinh_uninstall --on-event sinh-x_uninstall
+    _sinh_uninstall_bindings
+
+    functions --erase _sinh_uninstall _sinh_uninstall_bindings sinh_configure_bindings
+    complete --erase sinh_configure_bindings
+
+    set_color cyan
+    echo "sinh-x.fish uninstalled."
+    set_color normal
+end
